0b9a3acc97e76af4cdd864ff32702ab3b0b05235
S4r4h-O <128503216+S4r4h-O@users.noreply.github.com>
2025-04-27 22:53:26 -0300

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from http import client
-from forwardemail import forwardemail, aliases, encrypt, logs, account, emails
-from yaml import dump
-
-api_key = "44386d4569a9798ed23aee77"
-domain = "asnone.org"
-
-
